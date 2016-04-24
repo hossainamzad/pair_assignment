a8f95ec11aa6fc9aaaa1f589a56fd7f22d0a59a4
amzad hossain <amzad.hossain@baruchmail.cuny.edu>
2016-04-24 17:41:49 -0400

    addign a clock on the website

--- public/js/script.js
+++ public/js/script.js
@@ -10,10 +10,15 @@ $( "#tabs" ).tabs();
 
 })
   
-
-
-
 console.log("hi")
 
+
+
+function getCurrentDateTime(){
+
+	document.getElementById("clock") .innerHTML = Date ();
+}
+
+setInterval(getCurrentDateTime, 1000);
 
 
